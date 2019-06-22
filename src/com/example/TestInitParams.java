2829417bf78f6f6ac13fdadeb05e831f70937eca
Animation <763678968@qq.com>
2019-06-22 21:00:13 +0800

    TestInitParams_2

--- src/com/example/TestInitParams.java
+++ src/com/example/TestInitParams.java
@@ -14,8 +14,8 @@ public class TestInitParams extends HttpServlet {
         while (e.hasMoreElements()) {
             out.println("<br>param name = " + e.nextElement() + "<br>");
         }
-        out.println("main email is " + getServletConfig().getInitParameter("mainEmail"));
+        out.println("main email is " + getServletContext().getInitParameter("mainEmail"));
         out.println("<br>");
-        out.println("admin email is " + getServletConfig().getInitParameter("adminEmail"));
+        out.println("admin email is " + getServletContext().getInitParameter("adminEmail"));
     }
 }
