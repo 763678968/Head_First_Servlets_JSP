a4b99e86f3484e22b8f46a46b51f225d7e11eaa9
Animation <763678968@qq.com>
2019-06-24 17:17:28 +0800

    doGet_5

--- src/com/example/ListenerTester.java
+++ src/com/example/ListenerTester.java
@@ -10,16 +10,14 @@ public class ListenerTester extends HttpServlet {
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-
         out.println("test session attributes<br>");
-        HttpSession session = new request.getSession();
 
-        synchronized(session) {
-            getServletContext().setAttribute("foo", "22");
-            getServletContext().setAttribute("bar", "42");
+        HttpSession session = request.getSession();
 
-            out.println(session.getAttribute("foo"));
-            out.println(session.getAttribute("bar"));
+        if (session.isNew()) {
+            out.println("This is a new session.");
+        } else {
+            out.println("Welcome back!");
         }
 
     }
