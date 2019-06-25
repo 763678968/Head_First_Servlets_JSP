b740193a1d83a377f675180abc9fb6ef57fecc12
Animation <763678968@qq.com>
2019-06-25 16:26:16 +0800

    web.xml

--- src/com/example/MyServletContextListener.java
+++ src/com/example/MyServletContextListener.java
@@ -1,21 +1,21 @@
-package com.example;
-
-import javax.servlet.*;
-
-public class MyServletContextListener implements ServletContextListener {
-
-    public synchronized void contextInitialized (ServletContextEvent event) {
-
-        ServletContext sc = event.getServletContext();
-
-        String dogBreed = sc.getInitParameter("breed");
-
-        Dog d = new Dog(dogBreed);
-
-        sc.setAttribute("dog", d);
-    }
-
-    public void contextDestroyed (ServletContextEvent event) {
-        // nothing to do here
-    }
-}
+//package com.example;
+//
+//import javax.servlet.*;
+//
+//public class MyServletContextListener implements ServletContextListener {
+//
+//    public synchronized void contextInitialized (ServletContextEvent event) {
+//
+//        ServletContext sc = event.getServletContext();
+//
+//        String dogBreed = sc.getInitParameter("breed");
+//
+//        Dog d = new Dog(dogBreed);
+//
+//        sc.setAttribute("dog", d);
+//    }
+//
+//    public void contextDestroyed (ServletContextEvent event) {
+//        // nothing to do here
+//    }
+//}
