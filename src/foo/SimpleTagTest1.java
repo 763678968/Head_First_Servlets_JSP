dc92c3b0f4972529f9af9f65b4865411ce31a08e
Animation <763678968@qq.com>
2019-06-30 20:20:56 +0800

    simple.tld

--- src/foo/SimpleTagTest1.java
+++ src/foo/SimpleTagTest1.java
@@ -1,8 +1,14 @@
 package foo;
 
+import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
 // more imports needed
 
 public class SimpleTagTest1 extends SimpleTagSupport {
     // tag handler code here
+
+    public void doTag() throws JspException, IOException {
+        getJspContext().getOut().print("This is the lamest use of a custom tag");
+    }
 }
