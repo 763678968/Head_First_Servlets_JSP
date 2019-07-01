56b703ef5abdac87dc9576119413f29fbd39be5c
animation <763678968@qq.com>
2019-07-01 23:01:12 +0800

    CompressionResponseWrapper_724_modified

--- src/com/example/web/CompressionResponseWrapper.java
+++ src/com/example/web/CompressionResponseWrapper.java
@@ -57,3 +57,17 @@ public class CompressionResponseWrapper extends HttpServletResponseWrapper {
         return pw;
     }
 }
+
+class GZIPServletOutputStream extends ServletOutputStream {
+
+    GZIPOutputStream internalGzipOS;
+
+    /** 装饰器构造函数 */
+    GZIPServletOutputStream(ServletOutputStream sos) throws IOException {
+        this.internalGzipOS = new GZIPOutputStream(sos);
+    }
+
+    public void write(int param) throws java.io.IOException {
+        internalGzipOS.write(param);
+    }
+}
