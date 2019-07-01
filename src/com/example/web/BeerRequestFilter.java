82896b5d7f9d089bc83606bd0afb9e48e4be5b99
animation <763678968@qq.com>
2019-07-01 21:52:58 +0800

    BeerRequestFilter_707

---
+++ src/com/example/web/BeerRequestFilter.java
@@ -0,0 +1,31 @@
+package com.example.web;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+
+public class BeerRequestFilter implements Filter {
+
+    private FilterConfig fc;
+
+    public void init(FilterConfig config) throws ServletException {
+        this.fc = config;
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+
+        HttpServletRequest httpReq = (HttpServletRequest) req;
+
+        String name = httpReq.getRemoteUser();
+
+        if (name != null) {
+            fc.getServletContext().log("User " + name + " is updating");
+        }
+
+        chain.doFilter(req, resp);
+    }
+
+    public void destroy() {
+        // 完成清理工作
+    }
+}
