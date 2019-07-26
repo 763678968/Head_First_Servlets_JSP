a847cae5227b90b0696ca2879e073c773b420b15
animation <763678968@qq.com>
2019-07-26 22:33:04 +0800

    三层架构 补充Servlet QueryAllStudentsServlet.java

---
+++ src/QueryAllStudentsServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class QueryAllStudentsServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+
+        StudentService service = new StudentService();
+        List<Student> students = service.queryAllStudents();
+
+        System.out.println(students);
+    }
+}
