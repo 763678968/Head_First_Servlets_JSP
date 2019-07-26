89eab7b0a086cdd4e63e486fd250ce8d47d2080f
animation <763678968@qq.com>
2019-07-26 22:01:29 +0800

    三层架构 完善Service

--- src/StudentService.java
+++ src/StudentService.java
@@ -1,6 +1,33 @@
+import java.util.List;
+
 public class StudentService {
 
     StudentDao studentDao = new StudentDao();
+
+    // 根据学号查询学生
+    public Student queryStudentBySno(int sno) {
+        return studentDao.queryStudentBySno(sno);
+    }
+
+    // 查询全部学生
+    public List<Student> queryAllStudents() {
+        return studentDao.queryAllStudents();
+    }
+
+
+    public boolean updateStudentBySno(int sno, Student student) {
+        if (studentDao.isExist(sno)) {
+            return studentDao.deleteStudentBySno(sno);
+        }
+        return false;
+    }
+
+    public boolean deleteStudentBySno(int sno) {
+        if (studentDao.isExist(sno)) {
+            return studentDao.deleteStudentBySno(sno);
+        }
+        return false;
+    }
 
     public boolean addStudent(Student student) {
         if (!studentDao.isExist(student.getSno())) { // 不存在
