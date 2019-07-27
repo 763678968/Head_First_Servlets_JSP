682f5395aa5bd12c1fdce8de0a6787fefc8f7e68
animation <763678968@qq.com>
2019-07-27 23:17:14 +0800

    文件下载 UploadServlet.java modified

---
+++ src/UpAndDown/DownloadServlet.java
@@ -0,0 +1,39 @@
+package UpAndDown;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DownloadServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        // 获取需要下载的文件名
+        String fileName = request.getParameter(""); // from a href Servlet?a=b
+
+        // 下载文件：需要设置消息头
+        response.addHeader("content-Type", "application/octet-stream");
+        response.addHeader("content-Disposition", "attachment; filename=" + fileName);
+
+        // Servlet通过文件的地址，将文件转为输入流读到Servlet中
+        InputStream in = getServletContext().getResourceAsStream("/res/IMG_1157.png");
+
+        // 通过输出流将刚才已经转为输入流的文件输出给客户
+        ServletOutputStream out = response.getOutputStream();
+        byte[] bs = new byte[10];
+        int len = -1;
+        while ((len = in.read(bs)) != -1) {
+            out.write(bs, 0, len);
+        }
+        out.close();
+        in.close();
+    }
+}
